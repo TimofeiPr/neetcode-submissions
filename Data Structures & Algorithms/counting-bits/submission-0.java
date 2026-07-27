718fa1df88fffc790397416c5aae9981419f580c
TimofeiPr <tprakapchuk2@huskers.unl.edu>
2026-07-27 11:02:14 -0500

    Add: counting-bits - submission-0

---
+++ Data Structures & Algorithms/counting-bits/submission-0.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] countBits(int n) {
+        int[] output = new int[n + 1];
+        for (int i = 0; i <= n; i++) {
+            int count = 0;
+            for (char c : Integer.toBinaryString(i).toCharArray()) {
+                if (c == '1') {
+                    count ++;
+                }
+            }
+            output[i] = count;
+        }
+        return output;
+    }
+}
